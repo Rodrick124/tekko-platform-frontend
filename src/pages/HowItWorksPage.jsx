285cc1e3278c745780dc991e11d5757f474940f9
dirahno12 <dirahnombing@gmail.com>
2026-09-05 16:21:00 +0100

    Intègre l'identité Tekko et la page Comment ça marche

--- src/pages/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage.jsx
@@ -1,3 +1,12 @@
-import PagePlaceholder from '../components/common/PagePlaceholder.jsx'
-function HowItWorksPage() { return <PagePlaceholder title="Comment ça marche" /> }
+import ContactUnlockSection from '../features/how-it-works/components/ContactUnlockSection.jsx'
+import HowItWorksCta from '../features/how-it-works/components/HowItWorksCta.jsx'
+import HowItWorksFaq from '../features/how-it-works/components/HowItWorksFaq.jsx'
+import HowItWorksHero from '../features/how-it-works/components/HowItWorksHero.jsx'
+import ProcessStepsSection from '../features/how-it-works/components/ProcessStepsSection.jsx'
+import VerificationSection from '../features/how-it-works/components/VerificationSection.jsx'
+import WhatsAppExperienceSection from '../features/how-it-works/components/WhatsAppExperienceSection.jsx'
+
+function HowItWorksPage() {
+  return <><HowItWorksHero /><ProcessStepsSection /><ContactUnlockSection /><VerificationSection /><WhatsAppExperienceSection /><HowItWorksFaq /><HowItWorksCta /></>
+}
 export default HowItWorksPage
